d9d14e7ce74b0658b0c664c43ed30154d36acd01
E. Hannah <echannah631@gmail.com>
2021-02-19 10:27:32 -0800

    retrieved value of selected row from picker view

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -25,17 +25,28 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         currencyPicker.delegate = self
     }
     
+    //DataSource protocol
+    //Determines the number of columns in pickerView
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
-        //number of columns in pickerView
     }
     
+    //DataSource protocol
+    //This method asks for how many rows the pickerView will have
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return coinManager.currencyArray.count
     }
     
+    //Delegate protocol
+    //This method asks for the the title of each row in the pickerView, so it will be called (numberOfRowsInComponent) times
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return coinManager.currencyArray[row]
+    }
+    
+    //Delegate method
+    //Gets called every time the user scrolls the picker. Records the row number that was selected (when the scrolling stops)
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(coinManager.currencyArray[row])
     }
 
 
