98385a05dc4782b0c9cb65014a52dbf93221f850
E. Hannah <echannah631@gmail.com>
2021-02-19 16:23:47 -0800

    refactored view controller with extensions

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,10 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CoinManagerDelegate {
+class ViewController: UIViewController {
     
-    
-
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
@@ -25,32 +23,40 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         currencyPicker.delegate = self
         coinManager.delegate = self
     }
-    
-    //DataSource method
+}
+
+//MARK: - Picker View Data Source
+
+extension ViewController: UIPickerViewDataSource {
     //Determines the number of columns in pickerView
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
-    //DataSource method
     //This method asks for how many rows the pickerView will have
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return coinManager.currencyArray.count
     }
-    
-    //Delegate method
-    //This method asks for the the title of each row in the pickerView, so it will be called (numberOfRowsInComponent) times
+}
+
+//MARK: - Picker View Delegate
+
+extension ViewController: UIPickerViewDelegate {
+    //This method sets the title for each row in the pickerView, so it will be called (numberOfRowsInComponent) times
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return coinManager.currencyArray[row]
     }
     
-    //Delegate method
-    //Gets called every time the user scrolls the picker. Records the row number that was selected (when the scrolling stops)
+    //Gets called every time the user scrolls through the picker. Records the row number that was selected (when the scrolling stops)
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         let selectedCurrency = (coinManager.currencyArray[row])
         coinManager.getCoinPrice(for: selectedCurrency)
     }
-    
+}
+
+//MARK: - Coin Data Delegate
+
+extension ViewController: CoinManagerDelegate {
     func didCurrencyGetSelected(rate: String, currency: String) {
         DispatchQueue.main.async {
             self.bitcoinLabel.text = rate
@@ -62,4 +68,5 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         print(error)
     }
 }
+
 
